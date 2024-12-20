c71dddb3f012a2d9ba089c6c7fc7552901440b43
Gabriel Jacobini <gdavijac@gmail.com>
2024-12-20 15:18:59 -0300

    chore(untracked): add untracked files

---
+++ backends/Cashflow.Domain/Features/FinancialBoundaries/GetCategory/GetCategoryDtos.cs
@@ -0,0 +1,8 @@
+﻿using Cashflow.Domain.Entities;
+
+namespace Cashflow.Domain.Features.FinancialBoundaries.GetCategory;
+
+public record TransactionDto(long Id, string Description, DateTime DoneAt, string TransactionMethod, BankAccountDto? BankAccount);
+public record CategoryDto(long Id, string Name);
+public record BankAccountDto(long Id, string Name);
+
